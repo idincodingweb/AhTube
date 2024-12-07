066701f0cb2f23682300e333b0e98cc7506cee8c
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2024-12-07 21:04:12 +0700

    Update SearchResult.jsx

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -16,7 +16,7 @@ function SearchResultsPage() {
     const fetchResults = async () => {
       try {
         const response = await axios.get(
-          `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&type=video&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&type=video&key=${import.meta.env.VITE_KEY}`
         );
         setResults(response.data.items);
       } catch (error) {
